610c4d8b354a9415eb954142a0308dfac891a2c5
Arden Rasmussen <ardenisthebest@gmail.com>
2017-01-01 15:44:34 -0800

    Compleated matrix/vector math namespace in pessum

--- Core.cpp
+++ Core.cpp
@@ -7,17 +7,13 @@ int main(int argc, char *argv[]) {
   pessum::InitializePessumComponents();
   aequus::framework::SdlStartUp();
   aequus::video::CreateWindow("Aequus");
-  aequus::video::NewObject();
-  aequus::video::win->obj->CreateImgObj("white.png");
-  aequus::video::win->obj->Scale(100, 100);
-  aequus::video::win->obj->TerminateObject();
-  aequus::video::win->objects.erase(aequus::video::win->objects.begin());
   while (aequus::video::AllClose() == false) {
     aequus::Frame();
     if (aequus::input::events.size() > 0) {
-      if (aequus::input::events[0].type == aequus::input::KEYBOARD &&
-          aequus::input::events[0].key == 113) {
-        aequus::video::TerminateWindow();
+      if (aequus::input::events[0].type == aequus::input::KEYBOARD) {
+        if (aequus::input::events[0].key == 113) {
+          aequus::video::TerminateWindow();
+        }
       }
     }
   }
--- pessum_files/math.h
+++ pessum_files/math.h
@@ -7,11 +7,25 @@ namespace pessum {
 namespace math {
 extern int logloc;
 void InitializeMath();
+// Vector Functions
 double Dot(std::vector<double> a, std::vector<double> b);
 std::vector<double> Cross(std::vector<double> a, std::vector<double> b);
+// Matrix Functions
 double Determinate(std::vector<std::vector<double>> matrix);
 std::vector<std::vector<double>> MatrixMultipy(
     std::vector<std::vector<double>> a, std::vector<std::vector<double>> b);
+std::vector<std::vector<double>> TransposeMatrix(
+    std::vector<std::vector<double>> matrix);
+std::vector<std::vector<double>> ScalarMultipy(
+    double a, std::vector<std::vector<double>> b);
+std::vector<std::vector<double>> SumMatrix(std::vector<std::vector<double>> a,
+                                           std::vector<std::vector<double>> b);
+std::vector<std::vector<double>> DiffMatrix(std::vector<std::vector<double>> a,
+                                            std::vector<std::vector<double>> b);
+// Display Functions
+void DisplayVector(std::vector<double> a);
+void DisplayMatrix(std::vector<std::vector<double>> a);
+std::string ReduceDouble(double a);
 }
 }
 #endif
